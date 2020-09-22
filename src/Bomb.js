813cf6163a015ddf099cd0cc8da59f42b253ddd6
charlie763 <cwisoff@gmail.com>
2020-09-22 09:21:15 -0600

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+class Bomb extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  bombDisplay(){
+    if (this.state.secondsLeft > 0){
+      return(
+        <span>{this.state.secondsLeft} seconds left before I go boom!</span>
+      )
+    } else {
+      return <span>Boom!</span>
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        {this.bombDisplay()}
+      </div>
+    )
+  }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,30 @@
-// your ImageSlider code here!
+import React, { Component } from 'react'
+
+class ImageSlider extends Component {
+  constructor(){
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+  render(){
+    return(
+      <div>
+        <span>I am on slide {this.state.currentSlideIndex}</span>
+      </div>
+    )
+  }
+}
+
+export default ImageSlider
+
+
+
+// Let's pretend we're making an awesome slider for our new portfolio site.
+// Naturally, we'll use React to do so! We have to start somewhere, so in this lab
+// we'll just focus on setting up the initial state of the slider.
+
+// 1.  Its initial state should have a property called `currentSlideIndex` that
+//     starts at `0`.
+// 2.  It should only render out the text `'I am on slide <CURRENT_SLIDE>'`, where
+//     `<CURRENT_SLIDE>` is the value of `this.state.currentSlideIndex`.
